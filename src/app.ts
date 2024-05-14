8f9c70973d4571eb4a247dcf974d46e95b49b483
Leandro Perez <Leandro_perez03@outlook.com>
2024-05-13 21:49:37 -0400

    CORS 9

--- src/app.ts
+++ src/app.ts
@@ -9,21 +9,10 @@ import { FRONT_URL } from "./config";
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-    // origin: 
-    // [
-    //     'http://localhost:5173', 
-    //     'http://localhost:5173/sign-up', 
-    //     'http://localhost:5173/sign-in',
-    //     'https://citrux-test-client.netlify.app/sign-up', 
-    //     'https://citrux-test-client.netlify.app/sign-in',
-    //     'https://citrux-test-client.netlify.app',
-    //     'https://summaraizer-client.vercel.app/sign-in',
-    //     'https://summaraizer-client.vercel.app',
-    //     'https://summaraizer-client.vercel.app/sign-up'
-    // ],
+    origin: 'https://summaraizer-client.vercel.app',
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true,
-    // allowedHeaders: ['Content-Type', 'application/json']
+    allowedHeaders: ['Content-Type', 'application/json']
 }));
 app.use(cookieParser());
 app.use('/api', authRoutes);
