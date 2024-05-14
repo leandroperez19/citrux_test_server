637d8cb40ecdef94f37c0aef81b448a36a8d7857
Leandro Perez <Leandro_perez03@outlook.com>
2024-05-13 20:56:42 -0400

    Feat: Deploy

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,14 @@ import cookieParser from "cookie-parser";
 import { FRONT_URL } from "./config";
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(cors({
     origin: FRONT_URL,
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true
-}))
-app.use(cookieParser())
-app.use('/api', authRoutes)
-app.use('/api', summaryRoutes)
+}));
+app.use(cookieParser());
+app.use('/api', authRoutes);
+app.use('/api', summaryRoutes);
 
 export default app;
