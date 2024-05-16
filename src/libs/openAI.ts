d0ed791c66ec869016cd52e96ffc9730f726faea
Leandro Perez <Leandro_perez03@outlook.com>
2024-05-15 23:10:30 -0400

    Fix: More detailed question

--- src/libs/openAI.ts
+++ src/libs/openAI.ts
@@ -15,7 +15,7 @@ export const askQuestion = async (msg: string, article: string, previousQuestion
     But only if it refers to this article ${article}. 
     always return a JSON with this format:
     { 
-        code: if the question refers to the article or a previous question or answer: "success"| if it doesn't refers to the article a previous question or answer or is something random or you cant read the article: "error",
+        code: if the question refers to the article or a previous question or answer or have the same context of the conversation: "success"| if it doesn't refers to the article a previous question or answer or is something random or you cant read the article: "error",
         message: if code "success": your answer to the question | if code "error": "Sorry I can only answer question that have something to do with the article" or something more detailed
     }
     `
